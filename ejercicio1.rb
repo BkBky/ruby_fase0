b47b3e76f770c4c7a4320a8b2d932cc7001c4273
codeamac11 <codeamac11@gmail.com>
2017-01-09 17:02:16 -0600

    Ejercios de fase 0.Esta es la prueba de funcionamiento de git usando la terminal

---
+++ ejercicio1.rb
@@ -0,0 +1,69 @@
+title = "Ruby en altas"
+description = "rojo"
+number_of_likes = 30
+
+CONSTANT = 1
+
+street = "Sonora"
+number = 84
+state = "CDMX"
+city = "CDMX"
+zip = 00456
+
+#Ejercicio - Operaciones Aritméticas Básicas
+num1 = 10.0
+num2 = 5.0
+
+sum = (num1+num2)
+puts sum
+difference = (num1-num2)
+puts difference
+product = (num1*num2)
+puts product
+quotient = (num1/num2)
+puts quotient
+modulus = (num1%num2)
+puts modulus 
+doble = (num1**num2)
+puts doble
+
+#Ejercicio - Verdadero o Falso
+igual_que = 50 == 50
+igual_que = true
+
+menor_que = 23 < 30
+menor_que = true
+
+mayor_que = 54 > 40
+mayor_que = true
+
+menor_o_igual_que = 45 <= 45
+menor_o_igual_que = true
+
+mayor_o_igual_que = 56 >= 45
+mayor_o_igual_que = true
+
+diferente_que = 34 != 34
+diferente_que = false
+
+
+
+
+#Ejercicio - Operadores lógicos
+valor_1 = (1 < 7 || false) && (true || true)
+valor_1 = false
+valor_2 = !false && (!false || 50 != 5**10)
+valor_2 = true
+valor_3 = false || !(true || true)
+valor_3 = false
+
+#Ejercicio - Mensaje de Bienvenida
+
+
+first_name = "Roberto"
+last_name = "Monroe"
+gender = "M"
+
+puts "Welcome #{first_name}, your last_name is  #{last_name} and your gender is #{gender}"
+
+
---
+++ ejercicio3.rb
@@ -0,0 +1,21 @@
+ejercio3
+
+#Definiendo Variables
+
+
+
+#Define una variable tipo entero.
+variableEntero = 3
+VariableFlotante = 1.5
+variablebolean = true
+
+variablestring= "CDMX"
+
+
+#Define dos variables tipo flotante.
+
+#Define tres variables tipo boolean.
+
+#Define una variable y asígnale un arreglo que tenga valores tipo string.
+
+#Define una variable y asígnale un hash con tres elementos tipo entero.
